adfd2283e9f41b23d11106978a59e2cf6c641483
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2025-03-04 11:07:54 +0300

    create seeder data for user

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+// use DB;
 
 class UserTableSeeder extends Seeder
 {
@@ -12,6 +15,33 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table("users")->insert([
+            [
+                'name' => "Hailopia",
+                'username' => 'haileopia',
+                'email' => 'haileopia@gmail.com',
+                'password' => Hash::make('1234'),
+                'role' => 'admin',
+                'status' => 'active'
+            ],
+            // agent
+            [
+                'name' => "agent",
+                'username' => 'agent',
+                'email' => 'agent@gmail.com',
+                'password' => Hash::make('1234'),
+                'role' => 'agent',
+                'status' => 'active'
+            ],
+            // user
+            [
+                'name' => "user",
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('1234'),
+                'role' => 'user',
+                'status' => 'active'
+            ],
+        ]);
     }
 }
